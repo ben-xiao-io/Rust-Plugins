c236c1038e9fd9d9e516120202e5615f59c828d2
ben-xiao-io <benxiao778@gmail.com>
2023-06-13 20:09:19 -0400

    Add CS files

---
+++ RustServerCore/RustServerCore/rustserver/oxide/plugins/TestPlugin.cs
@@ -0,0 +1,314 @@
+﻿using Oxide.Core.Libraries.Covalence;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static OcclusionCulling;
+
+namespace Oxide.Plugins
+{
+    [Info("TestPLugin", "Bing Chilling", "0.1.0")]
+    [Description("Makes epic stuff happen")]
+
+    public class TestPlugin : CovalencePlugin
+    {
+        private static LayerMask MASKS = LayerMask.GetMask("Construction", "Default", "Deployed", "Resource", "Terrain", "Water", "World");
+        private const string BagPrefab = "assets/prefabs/deployable/sleeping bag/sleepingbag_leather_deployed.prefab";
+        private const string SphereEnt = "assets/prefabs/visualization/sphere.prefab";
+        private const string TransportHeliPrefab = "assets/content/vehicles/scrap heli carrier/scraptransporthelicopter.prefab";
+        private const string SwitchPrefab = "assets/prefabs/deployable/playerioents/simpleswitch/switch.prefab";
+        private const string AutoTurretPrefab = "assets/prefabs/npc/autoturret/autoturret_deployed.prefab";
+        private static HashSet<BaseEntity> zones =  new HashSet<BaseEntity>();
+        private static HashSet<AutoTurret> allTurrets = new HashSet<AutoTurret>();
+
+        private void Init()
+        {
+            Puts("RustPlugin is born!");
+        }
+
+        [Command("test")]
+        private void TestCommand(IPlayer player, string cmd, string[] args)
+        {
+            player.Reply("Test successful!");
+            RaycastHit hitInfo;
+            Vector3 playerPosition = new Vector3(player.Position().X, player.Position().Y, player.Position().Z);
+            if (Physics.Raycast(playerPosition, Vector3.down.normalized, out hitInfo, MASKS))
+            {
+                Vector3 groundPostion = new Vector3(playerPosition.x, hitInfo.point.y, playerPosition.z);
+                Vector3 groundVector = hitInfo.normal;
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, groundVector);
+
+                Puts(
+                    $"Ground at {playerPosition.x} , {playerPosition.y}, {playerPosition.z} " +
+                    $"with Rotation: {rotation.x}, {rotation.y}, {rotation.z}, {rotation.w}"
+                );
+
+                SleepingBag sleepingBag = GameManager.server.CreateEntity(BagPrefab, groundPostion, rotation) as SleepingBag;
+                if (sleepingBag == null)
+                {
+                    player.Reply($"Could not spawn bag!");
+                    return;
+                }
+
+                sleepingBag.Spawn();
+                return;
+
+            }
+
+            player.Reply("Unable to find ground!");
+        }
+
+
+        [Command("bubble")]
+        private void BubbleCommand(IPlayer player, string cmd, string[] args)
+        {
+            float radius = 15;
+            Vector3 playerPosition = new Vector3(player.Position().X, player.Position().Y, player.Position().Z);
+            BaseEntity sphere = GameManager.server.CreateEntity(SphereEnt, playerPosition, new Quaternion(), true);
+            SphereEntity ent = sphere.GetComponent<SphereEntity>();
+            ent.currentRadius = radius * 2;
+            ent.lerpSpeed = 0f;
+            sphere.Spawn();
+            //zones.Add(sphere);
+        }
+
+        [Command("clear")]
+        private void ClearZoneCommand(IPlayer player, string cmd, string[] args)
+        {
+            foreach(BaseEntity zone in zones)
+            {
+                zone.KillMessage();
+            }
+
+            zones.Clear();
+
+            UnityEngine.Object[] straysZones = GameObject.FindObjectsOfType(typeof(SphereEntity));
+            foreach(UnityEngine.Object straysZone in straysZones)
+            {
+                BaseEntity zoneAsEntity = (BaseEntity) straysZone;
+                zoneAsEntity.KillMessage();
+            }
+
+        }
+
+
+        [Command("debug")]
+        private void DebugCommand(IPlayer player, string cmd, string[] args)
+        {
+            BasePlayer playerObj = (BasePlayer) player.Object;
+            RaycastHit hitInfo;
+
+            if (!Physics.Raycast(playerObj.eyes.HeadRay(), out hitInfo))
+            {
+                player.Reply($"No entity found");
+                return;
+            }
+
+            player.Reply("Send out ray!");
+
+            BaseEntity entity = hitInfo.GetEntity();
+
+            if (!entity)
+            {
+                player.Reply("Entity not valid");
+                return;
+            }
+
+            player.Reply($"{entity.name}");
+
+        }
+
+        [Command("ac")]
+        private void ACCommand(IPlayer player, string cmd, string[] args)
+        {
+            BasePlayer baseplayer = (BasePlayer) player.Object;
+
+            // look pos
+            RaycastHit hitInfo;
+            if (!Physics.Raycast(baseplayer.eyes.HeadRay(), out hitInfo, Mathf.Infinity, MASKS))
+            {
+                player.Reply("Can't spawn AC130 here");
+                return;
+            }
+
+            var position = hitInfo.point + Vector3.up * 2f;
+            var transportHeli = (BaseVehicle) GameManager.server.CreateEntity(TransportHeliPrefab, baseplayer.transform.position);
+            if (transportHeli == null)
+            {
+                player.Reply("Unable to spawn AC13");
+                return;
+            }
+            transportHeli.OwnerID = baseplayer.userID;
+            transportHeli.rigidBody.useGravity = false;
+            transportHeli.Spawn();
+
+            StorageContainer fuelContainer = transportHeli.GetFuelSystem()?.GetFuelContainer();
+            ItemManager.CreateByName("lowgradefuel", 500)?.MoveToContainer(fuelContainer.inventory);
+
+            // add turret 
+            AutoTurret autoTurret1 = (AutoTurret) GameManager.server.CreateEntity(AutoTurretPrefab, transportHeli.transform.position);
+            if (autoTurret1 == null)
+            {
+                player.Reply("Could not place turret1 on AC130");
+                return;
+            }
+            autoTurret1.SetFlag(BaseEntity.Flags.Reserved8, true);
+            autoTurret1.SetParent(transportHeli);
+            autoTurret1.allowedContents = ItemContainer.ContentsType.Generic;
+            autoTurret1.pickup.enabled = false;
+            autoTurret1.transform.localPosition = new Vector3(0, 0.6f, 0f);
+            autoTurret1.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 180));
+            RemoveColliderProtection(autoTurret1);
+            autoTurret1.sightRange = 100f;
+            autoTurret1.eyePos.position += Vector3.left * 10f;
+            autoTurret1.RCEyes.position += Vector3.left * 10f;
+            autoTurret1.muzzlePos.position += Vector3.left * 10f;
+            autoTurret1.socketTransform.position += Vector3.left * 10f;
+            autoTurret1.gun_pitch.position += Vector3.left * 10f;
+            autoTurret1.gun_yaw.position += Vector3.left * 10f;
+
+            autoTurret1.eyePos.transform.position += Vector3.left * 10f;
+            autoTurret1.RCEyes.transform.position += Vector3.left * 10f;
+            autoTurret1.muzzlePos.transform.position += Vector3.left * 10f;
+            autoTurret1.socketTransform.transform.position += Vector3.left * 10f;
+            autoTurret1.gun_pitch.transform.position += Vector3.left * 10f;
+            autoTurret1.gun_yaw.transform.position += Vector3.left * 10f;
+
+            autoTurret1.rcTurnSensitivity = 0f;
+            autoTurret1.Spawn();
+            autoTurret1.aimCone = 0.1f;
+            autoTurret1.rcIdentifier = "test";
+            allTurrets.Add(autoTurret1);
+
+
+
+            //AutoTurret autoTurret2 = (AutoTurret)GameManager.server.CreateEntity(AutoTurretPrefab, transportHeli.transform.position);
+            //if (autoTurret1 == null)
+            //{
+            //    player.Reply("Could not place turret2 on AC130");
+            //    return;
+            //}
+            //autoTurret2.SetFlag(BaseEntity.Flags.Reserved8, true);
+            //autoTurret2.SetParent(transportHeli);
+            //autoTurret2.allowedContents = ItemContainer.ContentsType.Generic;
+            //autoTurret2.pickup.enabled = false;
+            //autoTurret2.transform.localPosition = new Vector3(-1.8f, 1f, -1.0f);
+            //RemoveColliderProtection(autoTurret2);
+            //autoTurret2.Spawn();
+
+            // auth turrets
+            var playerId = new PlayerNameID{userid = baseplayer.userID, username = baseplayer.displayName};
+            autoTurret1.authorizedPlayers.Add(playerId);
+            //autoTurret2.authorizedPlayers.Add(playerId);
+
+            // add switch
+            ElectricSwitch backTurretSwitch1 = (ElectricSwitch)GameManager.server.CreateEntity(SwitchPrefab);
+            if (backTurretSwitch1 == null)
+            {
+                player.Reply("Could not place switch on AC130");
+                return;
+            }
+            backTurretSwitch1.SetParent(autoTurret1);
+            backTurretSwitch1.transform.localPosition = new Vector3(0f, -0.6f, -0.3f);
+            backTurretSwitch1.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            backTurretSwitch1.pickup.enabled = false;
+            backTurretSwitch1._limitedNetworking = false;
+            RemoveColliderProtection(backTurretSwitch1);
+            backTurretSwitch1.Spawn();
+        }
+
+        [Command("turret")]
+        private void TurretCommand(IPlayer player, string cmd, string[] args)
+        {
+            BasePlayer baseplayer = (BasePlayer) player.Object;
+            AutoTurret autoTurret = (AutoTurret) GameManager.server.CreateEntity(AutoTurretPrefab, baseplayer.transform.position);
+            if (autoTurret == null)
+            {
+                player.Reply("Could not place turret1 on AC130");
+                return;
+            }
+
+            // autoTurret.RCEyes.localPosition += Vector3.up * 2f; // works
+            autoTurret.sightRange = 100f;
+
+            //autoTurret.socketTransform.localPosition += Vector3.up * 5f;
+            //autoTurret.socketTransform.position += Vector3.up * 5f;
+            //autoTurret.socketTransform.transform.localPosition += Vector3.up * 5f;
+            //autoTurret.socketTransform.transform.position += Vector3.up * 5f;
+            autoTurret.dropsLoot = false;
+            autoTurret.Spawn();
+
+            //switch
+
+            ElectricSwitch backTurretSwitch1 = (ElectricSwitch)GameManager.server.CreateEntity(SwitchPrefab);
+            if (backTurretSwitch1 == null)
+            {
+                player.Reply("Could not place switch on AC130");
+                return;
+            }
+            backTurretSwitch1.SetParent(autoTurret);
+            backTurretSwitch1.transform.localPosition = new Vector3(0f, -0.6f, -0.3f);
+            backTurretSwitch1.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            backTurretSwitch1.pickup.enabled = false;
+            backTurretSwitch1._limitedNetworking = false;
+           
+            RemoveColliderProtection(backTurretSwitch1);
+            backTurretSwitch1.Spawn();
+
+            return;
+        }
+
+        [Command("switch")]
+        private void SwitchCommand(IPlayer player, string cmd, string[] args)
+        {
+            BasePlayer baseplayer = (BasePlayer)player.Object;
+            // add switch
+            ElectricSwitch backTurretSwitch = (ElectricSwitch)GameManager.server.CreateEntity(SwitchPrefab);
+            if (backTurretSwitch == null)
+            {
+                player.Reply("Could not place switch on AC130");
+                return;
+            }
+            var switchLocation = baseplayer.transform.position + new Vector3(0f, 1f, 0f);
+            backTurretSwitch.transform.localPosition = switchLocation;
+            backTurretSwitch.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            backTurretSwitch.pickup.enabled = false;
+            backTurretSwitch._limitedNetworking = false;
+            RemoveColliderProtection(backTurretSwitch);
+            backTurretSwitch.Spawn();
+            player.Reply("Made switch!");
+        }
+
+        private static void RemoveColliderProtection(BaseEntity colliderEntity)
+        {
+
+            foreach (var meshCollider in colliderEntity.GetComponentsInChildren<MeshCollider>())
+            {
+                UnityEngine.Object.DestroyImmediate(meshCollider);
+            }
+
+            UnityEngine.Object.DestroyImmediate(colliderEntity.GetComponent<GroundWatch>());
+        }
+
+
+        private void OnDoorOpened(Door door, BasePlayer player)
+        {
+            if (!door.IsLocked())
+            {
+                player.ChatMessage($"Hey! You should knock first");
+            }
+
+            return;
+        }
+
+        private object OnItemCraft(ItemCraftTask task, BasePlayer player, Item item)
+        {
+            task.endTime = 1f;
+            player.ChatMessage("Item has been instantly crafted!");
+            return null;
+        }
+    }
+}
---
+++ RustServerCore/RustServerCore/rustserver/oxide/plugins/Warthog.cs
@@ -0,0 +1,373 @@
+﻿using Newtonsoft.Json;
+using Oxide.Core.Libraries.Covalence;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Warthog", "Bing Chilling", "0.1.0")]
+    [Description("Spawn warthog machine.")]
+
+    public class Warthog : CovalencePlugin
+    {
+        public string SpawnPermission = "warthog.spawn";
+        public string CoolDownPermission = "warthog.cooldown";
+        private static LayerMask MASKS = LayerMask.GetMask("Construction", "Default", "Deployed", "Resource", "Terrain", "Water", "World");
+        private const string TransportHeliPrefab = "assets/content/vehicles/scrap heli carrier/scraptransporthelicopter.prefab";
+        private const string SwitchPrefab = "assets/prefabs/deployable/playerioents/simpleswitch/switch.prefab";
+        private const string AutoTurretPrefab = "assets/prefabs/npc/autoturret/autoturret_deployed.prefab";
+
+        private static Dictionary<ulong, float> coolDownMap = new Dictionary<ulong, float>();
+        private static HashSet<AutoTurret> warthogTurrets = new HashSet<AutoTurret>();
+        private static Dictionary<ulong, BaseVehicle> warthogMap = new Dictionary<ulong, BaseVehicle>();
+        private static HashSet<BaseVehicle> warthogs = new HashSet<BaseVehicle>();
+
+        private static Configuration cfg;
+
+        private class Configuration
+        {
+            [JsonProperty(PropertyName = "Spawn Cooldown")]
+            public float SpawnCooldown = 30f;
+
+            [JsonProperty(PropertyName = "Maximum Distance Allowed For Spawning Warthog")]
+            public float MaxSpawnDistance = 50f;
+
+            [JsonProperty(PropertyName = "Spawn Warthog With Fuel")]
+            public bool SpawnWithFuel = true;
+
+            [JsonProperty(PropertyName = "Warthog Automatic Fire Rate")]
+            public float AutomaticFireRate = 0.001f;
+
+            [JsonProperty(PropertyName = "Warthog Semi Automatic Fire Rate")]
+            public float SemiAutomaticFireRate = 0.5f;
+
+            [JsonProperty(PropertyName = "Warthog Aim Cone")]
+            public float AimCone = 1f;
+        }
+
+        public class WarthogMetaData : MonoBehaviour
+        {
+            public ulong ownerID;
+            public ulong entityID;
+        }
+
+        public class WarthogTurretMetaData : MonoBehaviour
+        {
+            public ulong ownerID;
+            public ulong entityID;
+
+            public float defaultRepeatDelay = 0.5f;
+            public float defaultAiAimCone = 1f;
+        }
+
+        #region Config
+
+        private void SaveConfig(Configuration cfg) => Config.WriteObject(cfg, true);
+
+        private bool LoadConfigVariables()
+        {
+            base.LoadConfig();
+            try
+            {
+                cfg = Config.ReadObject<Configuration>();
+                if (cfg == null) throw new Exception();
+            }
+            catch
+            {
+                Console.Error.WriteLine("Configuration failed to load. Loading default configuration instead.");
+                LoadDefaultConfig();
+                return false;
+            }
+
+            SaveConfig(cfg);
+            return true;
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            PrintWarning("Creating new config.");
+            cfg = new Configuration();
+            SaveConfig(cfg);
+        }
+
+        #endregion
+
+        #region Init
+
+        private void Init()
+        {
+            LoadConfigVariables();
+            permission.RegisterPermission(SpawnPermission, this);
+            permission.RegisterPermission(CoolDownPermission, this);
+            Puts("Warthog has been initialized.");
+        }
+
+        private void Unload()
+        {
+            ClearAllWarthogs();
+        }
+
+        #endregion
+
+        #region Commands
+
+        [Command("warthog")]
+        private void WarthogCommand(IPlayer player, string cmd, string[] args)
+        {
+            if (!player.HasPermission(SpawnPermission))
+            {
+                player.Reply("No permission to spawn warthog");
+                return;
+            };
+
+            BasePlayer basePlayer = (BasePlayer)player.Object;
+            ulong playerID = basePlayer.userID;
+            float lastUsed;
+
+            if (!coolDownMap.TryGetValue(playerID, out lastUsed))
+            {
+                coolDownMap.Add(playerID, Time.time);
+                lastUsed = 0f;
+            };
+
+            float currTime = Time.time;
+            float canUseTime = (lastUsed + cfg.SpawnCooldown);
+
+            if (!player.HasPermission(CoolDownPermission))
+            {
+                if (currTime < canUseTime)
+                {
+                    float remainingTime = canUseTime - currTime;
+                    string displayTime = remainingTime.ToString("0.0 second(s)");
+                    player.Reply($"Cannot spawn warthog for another {displayTime}");
+                    return;
+                }
+            }
+
+            if (!SpawnWarthog(player))
+            {
+                player.Reply("[ERROR] Couldn't spawn Warthog.");
+                return;
+            }
+
+            coolDownMap[playerID] = Time.time;
+            return;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private bool SpawnWarthog(IPlayer player)
+        {
+            BasePlayer basePlayer = (BasePlayer) player.Object;
+
+            RaycastHit hitInfo;
+            if (!Physics.Raycast(basePlayer.eyes.HeadRay(), out hitInfo, Mathf.Infinity, MASKS)) return false;
+
+            var position = hitInfo.point + Vector3.up * 2f;
+            var warthog = (BaseVehicle) GameManager.server.CreateEntity(TransportHeliPrefab, position);
+            if (warthog == null) return false;
+            warthog.OwnerID = basePlayer.userID;
+            warthog.Spawn();
+
+            if (cfg.SpawnWithFuel)
+            {
+                StorageContainer fuelContainer = warthog.GetFuelSystem()?.GetFuelContainer();
+                if (fuelContainer != null)
+                {
+                    ItemManager.CreateByName("lowgradefuel", 500)?.MoveToContainer(fuelContainer.inventory);
+                }
+            }
+
+            WarthogMetaData metadata = warthog.gameObject.AddComponent<WarthogMetaData>();
+            metadata.entityID = warthog.net.ID.Value;
+            metadata.ownerID = basePlayer.userID;
+
+            if (!SpawnTurret(warthog, basePlayer))
+            {
+                warthog.KillMessage();
+                player.Reply("Unable to spawn Warthog turrets");
+                return false;
+            }
+
+            BaseVehicle prevWarthog;
+            if (warthogMap.TryGetValue(basePlayer.userID, out prevWarthog))
+            {
+                if (!prevWarthog.IsDestroyed) prevWarthog.KillMessage();
+                warthogMap.Remove(basePlayer.userID);
+            }
+
+            warthogMap.Add(basePlayer.userID, warthog);
+            warthogs.Add(warthog);
+
+            return true;
+        }
+
+        private bool SpawnTurret(BaseVehicle warthog, BasePlayer basePlayer)
+        {
+            AutoTurret autoTurretRight = (AutoTurret) GameManager.server.CreateEntity(AutoTurretPrefab, warthog.transform.position);
+            if (autoTurretRight == null) return false;
+
+            autoTurretRight.SetFlag(BaseEntity.Flags.Reserved8, true);
+            autoTurretRight.SetParent(warthog);
+            autoTurretRight.pickup.enabled = false;
+            autoTurretRight._limitedNetworking = false;
+            autoTurretRight.transform.localPosition = new Vector3(1.8f, 1f, -1.0f);
+            autoTurretRight.transform.localRotation = Quaternion.Euler(new Vector3(0f, 90f, 0));
+            RemoveColliderProtection(autoTurretRight);
+
+            autoTurretRight.dropsLoot = false;
+            autoTurretRight.sightRange = 50f;
+            autoTurretRight.Spawn();
+
+            if (!AddSwitch(autoTurretRight))
+            {
+                autoTurretRight.KillMessage();
+                return false;
+            }
+
+            WarthogTurretMetaData metadata = autoTurretRight.gameObject.AddComponent<WarthogTurretMetaData>();
+            metadata.entityID = autoTurretRight.net.ID.Value;
+            metadata.ownerID = basePlayer.userID;
+
+            warthogTurrets.Add(autoTurretRight);
+
+            AuthorizePlayer(autoTurretRight, basePlayer);
+
+            return true;
+        }
+
+        private bool AddSwitch(AutoTurret autoTurret)
+        {
+            ElectricSwitch electricalSwitch = (ElectricSwitch) GameManager.server.CreateEntity(SwitchPrefab, autoTurret.transform.localPosition);
+            if (electricalSwitch == null) return false;
+            electricalSwitch.transform.localPosition = new Vector3(0f, -0.60f, 0.3f);
+            electricalSwitch.transform.localRotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+            electricalSwitch.pickup.enabled = false;
+            electricalSwitch._limitedNetworking = false;
+            electricalSwitch.SetParent(autoTurret);
+            RemoveColliderProtection(electricalSwitch);
+            electricalSwitch.Spawn();
+
+            return true;
+        }
+
+        private static void RemoveColliderProtection(BaseEntity colliderEntity)
+        {
+            foreach (MeshCollider meshCollider in colliderEntity.GetComponentsInChildren<MeshCollider>())
+            {
+                UnityEngine.Object.DestroyImmediate(meshCollider);
+            }
+
+            UnityEngine.Object.DestroyImmediate(colliderEntity.GetComponent<GroundWatch>());
+        }
+
+        private void AuthorizePlayer(AutoTurret autoTurret, BasePlayer player)
+        {
+            var playerId = new PlayerNameID { userid = player.userID, username = player.displayName };
+            autoTurret.authorizedPlayers.Add(playerId);
+
+            RelationshipManager.PlayerTeam team = player.Team;
+            if (team == null) return;
+
+            foreach(ulong memberId in team.members)
+            {
+                BasePlayer memberBase = BasePlayer.FindByID(memberId);
+                if (memberBase.IsRealNull()) continue;
+
+                var teamMemberNameId = new PlayerNameID { userid = memberBase.userID, username = memberBase.displayName };
+                autoTurret.authorizedPlayers.Add(teamMemberNameId);
+            }
+        }
+
+        private void ClearAllWarthogs()
+        {
+            Puts($"Clearing {warthogMap.Count} warthog(s)...");
+            foreach (BaseVehicle warthog in warthogMap.Values)
+            {
+                warthog.KillMessage();
+            }
+
+            warthogMap.Clear();
+            warthogs.Clear();
+            warthogTurrets.Clear();
+            coolDownMap.Clear();
+        }
+
+        #endregion
+
+        #region Hooks
+
+        private object OnTurretTarget(AutoTurret turret, BaseCombatEntity entity)
+        {
+            BaseVehicle warthog = (BaseVehicle) turret.GetParentEntity();
+            if (warthog == null) return null;   
+
+            BaseProjectile weapon = (BaseProjectile) turret.AttachedWeapon;
+            if (weapon == null) return null;
+
+            weapon.repeatDelay = (weapon.isSemiAuto) ? cfg.SemiAutomaticFireRate : cfg.AutomaticFireRate;
+            weapon.aiAimCone = cfg.AimCone;
+            return null;
+        }
+
+        private object CanMountEntity(BasePlayer player, BaseMountable entity)
+        {
+            BaseVehicle baseVehicle = entity.VehicleParent();
+            if (baseVehicle == null) return null;
+
+            WarthogMetaData metadata;
+            if (!baseVehicle.TryGetComponent<WarthogMetaData>(out metadata)) return null;
+
+            if (!warthogs.Contains(baseVehicle))
+            {
+                baseVehicle.KillMessage(); 
+                player.ChatMessage("This Warthog is not owned by you or another player. Removing Warthog...");
+                return null;
+            }
+
+            if (player.userID != metadata.ownerID)
+            {
+                player.ChatMessage("No permission to enter into this Warthog.");
+                return false;
+            }
+
+            return null;
+        }
+
+
+        private void OnEntityDeath(BaseCombatEntity entity, HitInfo info)
+        {
+            WarthogMetaData metadata;
+            if (!entity.TryGetComponent<WarthogMetaData>(out metadata)) return;
+            BasePlayer owner = BasePlayer.FindByID(metadata.ownerID);
+            owner.ChatMessage("Your warthog has been destroyed!");
+        }
+
+        private object OnTurretShutdown(AutoTurret turret)
+        {
+            WarthogTurretMetaData warthogTurretMetaData;
+            if (!turret.TryGetComponent<WarthogTurretMetaData>(out warthogTurretMetaData)) return null;
+
+            BaseProjectile weapon = (BaseProjectile)turret.AttachedWeapon;
+            if (weapon == null) return null;
+
+            weapon.repeatDelay = warthogTurretMetaData.defaultRepeatDelay;
+            weapon.aiAimCone = warthogTurretMetaData.defaultAiAimCone;
+
+            return null;
+        }
+
+
+        private object OnPlayerDeath(BasePlayer player, HitInfo hitInfo)
+        {
+            Puts(hitInfo.boneName);
+            return null;
+        }
+
+        #endregion
+    }
+}
